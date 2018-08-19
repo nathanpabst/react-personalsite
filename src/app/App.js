014afd7a27b28010beaef0b8d272a8b9ef1047ec
Nathan Pabst <nathan.pabst@gmail.com>
2018-08-19 15:57:56 -0500

    setup complete

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
+// import {Route, BrowserRouter, Redirect, Switch} from 'react-router-dom';
+// import firebase from 'firebase';
+
 import './App.css';
 
-class App extends Component {
+import fbConnection from '../firebaseRequests/connection';
+fbConnection();
+
+class App extends React.Component {
   render () {
     return (
       <div className="App">
@@ -11,6 +17,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button className="btn btn-danger">ImaButton</button>
       </div>
     );
   }
